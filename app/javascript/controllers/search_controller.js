6a7f7e1ce56d141808bf53084c34e9343edadbf0
leonardodfelix <leonardo.doria.felix1983@gmail.com>
2023-04-15 10:07:49 -0300

    add and bind search to view

--- app/javascript/controllers/search_controller.js
+++ app/javascript/controllers/search_controller.js
@@ -2,6 +2,24 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="search"
 export default class extends Controller {
+  static targets = ["title", "content"];
+
   connect() {
+    console.log("Stimulus search controller: connected", this.element);
+  }
+
+  filterPosts(event) {
+    const searchTerm = event.target.value.toLowerCase();
+
+    this.titleTargets.forEach((title) => {
+      const postContent = title.nextElementSibling.textContent.toLowerCase();
+      const postTitle = title.textContent.toLowerCase();
+
+      if (postContent.includes(searchTerm) || postTitle.includes(searchTerm)) {
+        title.parentElement.style.display = "";
+      } else {
+        title.parentElement.style.display = "none";
+      }
+    });
   }
 }
